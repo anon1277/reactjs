82a34beda5c1cf5b690c6585e6f316cdc2e2cd02
anon1277 <vish2355@gmail.com>
2024-07-15 21:25:14 +0530

    State with Functional Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import User  from './User';
+import User from './User';
 import UserClass from './UserClass';
 
 function App() {
+  // State variable to manage data
+  const [data, setData] = useState("hack");
 
+  // Regular variable to hold username
   let userName = "hack1277";
-  function apple(){
+
+  // Function to handle button click
+  function apple() {
+    // Update regular variable (userName)
     userName = "email";
+
+    // Update state variable (data)
+    setData("hack1277");
+
+    // Alert user
     alert("Form is Submitted");
   }
+
   return (
     <div className="App">
-
-      
+      {/* Render User component */}
       <User />
 
+      {/* Render UserClass component */}
       <UserClass />
-      <h1>{userName}</h1>
+
+      {/* Display state variable (data) */}
+      <h1>{data}</h1> {/* value will update due to state */}
+
+      {/* Display regular variable (userName) */}
+      <h1>{userName}</h1> {/* value will not update */}
+
+      {/* Button to trigger the apple function */}
       <button onClick={apple}>Submit</button>
     </div>
   );
